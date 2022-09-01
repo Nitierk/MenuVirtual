d1b1b1b4149fbd0cecfb1af5ddcd80a339485332
Escola <105940525+Nitierk@users.noreply.github.com>
2022-09-01 16:09:37 -0300

    Criação do Crud

---
+++ Projeto Lunary/Controllers/RestauranteController.cs
@@ -0,0 +1,40 @@
+﻿using Projeto_Lunary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto_Lunary.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        // GET: Restaurante
+        BDLunary bd = new BDLunary();
+        public ActionResult Index()
+        {
+            return View(bd.Restaurante.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string nome, float preco, string descricao, float precopromocao, string categoria, string subCategoria)
+        {
+            Restaurante novoRestaurante = new Restaurante();
+            novoRestaurante.RESTANOME = nome;
+            novoRestaurante.RESTAPRECO = preco;
+            novoRestaurante.RESTADESCRICAO = descricao;
+            novoRestaurante.RESTAPREPROMOCAO = precopromocao;
+            novoRestaurante.RESTACATEGORIA = categoria;
+            novoRestaurante.RESTASUBCATEGORIA = categoria;
+
+            bd.Restaurante.Add(novoRestaurante);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
