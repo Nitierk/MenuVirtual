25546d4027760920ff91756a652da99bceb932a1
Kauan <105940525+Nitierk@users.noreply.github.com>
2022-09-22 17:15:33 -0300

    Ta na metade

---
+++ Projeto Lunary/Scripts/Menu/HeartClick.js
@@ -0,0 +1,40 @@
+﻿function Like(id) {
+    var id = id;
+    var url = "/Restaurante/ContagemLikes";
+    var status;
+    let el = document.getElementById(id);
+    //Botão curtido
+    if (el.classList.contains('heart-active')) {
+        el.classList.remove('heart-active');
+        status = false;
+        $.post(url, { id: id, status: status }, function (data) {
+
+        });
+        var qtdLikesBefore = document.getElementById(id + "-Curtida")
+        var qtdCurtidas = qtdLikesBefore.innerHTML;
+        console.log(qtdCurtidas)
+        qtdLikesBefore.remove()
+        var Curti = document.createElement('span');
+        Curti.setAttribute("class", "number");
+        Curti.innerHTML = parseInt(qtdCurtidas) - 1;
+        document.getElementById(id + "-Curtidadiv").appendChild(Curti);
+    }
+    //Botão sem curtir
+    else {
+        el.classList.add('heart-active');
+        status = true;
+        $.post(url, { id: id, status: status }, function (data) {
+
+        });
+        var qtdLikesBefore = document.getElementById(id + "-Curtida")
+        var qtdCurtidas = qtdLikesBefore.innerHTML;
+        console.log(qtdCurtidas)
+        qtdLikesBefore.remove()
+        var Curti = document.createElement('span');
+        Curti.setAttribute("class", "number");
+        Curti.innerHTML = parseInt(qtdCurtidas) + 1;
+        document.getElementById(id + "-Curtidadiv").appendChild(Curti);
+    }
+
+
+};
