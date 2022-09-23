1d2835cd84d627d2cd147fb88306ea4f6a170709
Kauan <105940525+Nitierk@users.noreply.github.com>
2022-09-23 15:36:33 -0300

    Modal Funcionando novamente

--- Projeto Lunary/Scripts/Menu/ModalClick.js
+++ Projeto Lunary/Scripts/Menu/ModalClick.js
@@ -1,1 +1,9 @@
-﻿
+﻿function OpenModal(id) {
+    
+    console.log(id)
+    $("#conteudoModal").load("/Restaurante/Detalhes/" + id,
+        function () {
+            $('#myModal').modal("show")
+        }
+    );
+}
