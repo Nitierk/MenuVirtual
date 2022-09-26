33a59eee9f5f0d937d7d2b455cb2d4674484b7a2
Kauan <105940525+Nitierk@users.noreply.github.com>
2022-09-26 19:17:23 -0300

    Edit Img Vindo do banco
    Mas não esta mudando

--- Projeto Lunary/Scripts/Admin/InputPicture.js
+++ Projeto Lunary/Scripts/Admin/InputPicture.js
@@ -26,7 +26,45 @@
         } else {
             pictureImage.innerHTML = pictureImageTxt;
         }
-    });
+    }); 
+}
+function InputImgEdit(ImgSrc) {
+    const inputFile = document.querySelector("#imagem");
+    const pictureImage = document.querySelector(".imagem");
+    const pictureImageTxt = "Escolha uma Imagem";
+    pictureImage.innerHTML = pictureImageTxt;
 
-    
+    inputFile.addEventListener("change", function (e) {
+        const inputTarget = e.target;
+        const file = inputTarget.files[0];
+        
+        if (file) {
+            const reader = new FileReader();
+
+            reader.addEventListener("load", function (e) {
+                const readerTarget = e.target;
+
+                const img = document.createElement("img");
+                img.src = ImgSrc;
+                img.classList.add("imagem");
+
+                pictureImage.innerHTML = "";
+                pictureImage.appendChild(img);
+            });
+
+            reader.readAsDataURL(file);
+        } else {
+            pictureImage.innerHTML = pictureImageTxt;
+        }
+    });
+        
+    if (ImgSrc != null) {
+                const img = document.createElement("img");
+                img.src = ImgSrc;
+                img.classList.add("imagem");
+                pictureImage.innerHTML = "";
+  
+        } else {
+            pictureImage.innerHTML = pictureImageTxt;
+        }
 }
