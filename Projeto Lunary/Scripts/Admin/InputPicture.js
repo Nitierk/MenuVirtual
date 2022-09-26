3442b538e56826a1f7047d0e628f6e6e27d92248
Kauan <105940525+Nitierk@users.noreply.github.com>
2022-09-26 18:00:43 -0300

    Create Input

---
+++ Projeto Lunary/Scripts/Admin/InputPicture.js
@@ -0,0 +1,28 @@
+﻿const inputFile = document.querySelector("#imagem");
+const pictureImage = document.querySelector(".imagem");
+const pictureImageTxt = "Choose an image";
+pictureImage.innerHTML = pictureImageTxt;
+
+inputFile.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function (e) {
+            const readerTarget = e.target;
+
+            const img = document.createElement("img");
+            img.src = readerTarget.result;
+            img.classList.add("imagem");
+
+            pictureImage.innerHTML = "";
+            pictureImage.appendChild(img);
+        });
+
+        reader.readAsDataURL(file);
+    } else {
+        pictureImage.innerHTML = pictureImageTxt;
+    }
+});
