2c0de7ffc67e80bd3ac0f322965481c539865eb0
Escola <105940525+Nitierk@users.noreply.github.com>
2022-10-04 19:01:02 -0300

    QrCode Funcionando

--- Projeto Lunary/Controllers/AdminController.cs
+++ Projeto Lunary/Controllers/AdminController.cs
@@ -199,22 +199,6 @@ namespace Projeto_Lunary.Controllers
             return View();
         }
 
-        public ActionResult QR(string qrcode)
-        {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
-                QRCodeData qrCodeData = qRCodeGenerator.CreateQrCode(qrcode, QRCodeGenerator.ECCLevel.Q);
-                QRCode qrCode = new QRCode(qrCodeData);
-
-                using (Bitmap bitmap = qrCode.GetGraphic(20))
-                {
-                    bitmap.Save(ms, ImageFormat.Png);
-                    ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
-                }
-            }
-            return View();
-        }
      
     }
 }
