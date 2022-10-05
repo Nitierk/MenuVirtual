06a90acd16a02e31d8a258c5d1d7e5f56632cde1
Escola <105940525+Nitierk@users.noreply.github.com>
2022-10-05 17:17:02 -0300

    Não vai da para fazer por enquato :(

--- Projeto Lunary/Scripts/Menu/ModalClick.js
+++ Projeto Lunary/Scripts/Menu/ModalClick.js
@@ -1,9 +1,37 @@
 ﻿function OpenModal(id) {
     
-    console.log(id)
+    /*console.log(id)*/
     $("#conteudoModal").load("/Restaurante/Detalhes/" + id,
+
+        
+
         function () {
             $('#myModal').modal("show")
+
+            
         }
     );
+
+    
+
+/*
+    try {
+        var titu = document.getElementById("TituloCat");
+        titu.remove();
+        var Categ = document.createElement('h3'); // is a node
+        Categ.setAttribute("class", "Categoria");
+        Categ.setAttribute("id", "TituloCat");
+
+        Categ.innerHTML = c;
+        document.getElementById('Categoria').appendChild(Categ);
+
+    } catch (e) {
+        var Categ = document.createElement('h3'); // is a node
+        Categ.setAttribute("id", "TituloCat");
+        Categ.setAttribute("class", "Categoria");
+        Categ.innerHTML = c;
+        document.getElementById('Categoria').appendChild(Categ);
+    }*/
+
+
 }
