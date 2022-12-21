a82a6d2c54f73447e9855c5d942ce7ecec74a096
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-12-21 14:28:49 -0300

    update

--- Projeto Lunary/Scripts/Menu/TourScript.js
+++ Projeto Lunary/Scripts/Menu/TourScript.js
@@ -3,29 +3,41 @@
         {
             element: "#ToggleTheme",
             title: "Modo Claro/Escuro",
-            content: "É possível alterar o tema do site."
+            content: "É possível alterar o tema do site.",
+            placement: "bottom",
+            backdrop:false,
+            smartPlacement:false
         },
         {
             element: "#Eventos",
             title: "Eventos.",
-            content: "Futuros eventos do estabelcimento"
+            content: "Futuros eventos do estabelcimento",
+            placement: "bottom",
+            smartPlacement:false
         },
         {
             element: "#maisCurtidos",
             title: "Top 5 pratos.",
-            content: "Rankiamento dos Pratos mais curtidos do estabelecimento."
+            content: "Rankiamento dos Pratos mais curtidos do estabelecimento.",
+            placement: "bottom",
+            smartPlacement:false
         },
         {
-            element: "#filterCategoria",
+            element: "#FiltroCategorias",
             title: "Filtro das Categorias.",
-            content: "Navegue pelas categorias de forma fácil."
+            content: "Navegue pelas categorias de forma fácil.",
+            placement: "bottom",
+            smartPlacement:false
         },
         {
-            element: "#filterCategoria",
-            title: "Filtro das Categorias.",
-            content: "Navegue pelas categorias de forma fácil."
+            element: "#progress",
+            title: "Volte para o Topo.",
+            content: "Volte para o topo da página rápidamente.",
+            placement: "left",
+            smartPlacement:false
         }
-    ]
+    ],    
+    backdrop:true,
 });
 
 //-> Inicializa o tour.
