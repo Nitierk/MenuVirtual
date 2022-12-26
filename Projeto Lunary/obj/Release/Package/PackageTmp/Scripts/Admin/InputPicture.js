479ce2a43b016ac5fb45b0028aace09add285fa6
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-12-26 17:23:29 -0300

    Consertado

---
+++ Projeto Lunary/obj/Release/Package/PackageTmp/Scripts/Admin/InputPicture.js
@@ -0,0 +1,72 @@
+﻿function InputImgCreate() {
+    const inputFile = document.querySelector("#imagem");
+    const pictureImage = document.querySelector(".imagem");
+    const pictureImageTxt = "Escolha uma Imagem";
+    pictureImage.innerHTML = pictureImageTxt;
+
+    inputFile.addEventListener("change", function (e) {
+        const inputTarget = e.target;
+        const file = inputTarget.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.addEventListener("load", function (e) {
+                const readerTarget = e.target;
+
+                const img = document.createElement("img");
+                img.src = readerTarget.result;
+                img.classList.add("imagem");
+
+                pictureImage.innerHTML = "";
+                pictureImage.appendChild(img);
+            });
+
+            reader.readAsDataURL(file);
+        } else {
+            pictureImage.innerHTML = pictureImageTxt;
+        }
+    }); 
+}
+function InputImgEdit() {
+    const inputFile = document.querySelector("#imagem");
+
+    inputFile.addEventListener("change", function (e) {
+       
+        
+
+        const inputTarget = e.target;
+        const file = inputTarget.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.addEventListener("load", function (e) {
+                
+                try {
+                    const imgDb = document.getElementById("imagemEdit")
+                    imgDb.remove()
+                } catch (e) {
+
+                }
+                
+                const readerTarget = e.target;
+
+                const img = document.createElement("img");
+                img.src = readerTarget.result;
+                img.classList.add("imagem");
+
+                pictureImage.innerHTML = "";
+                pictureImage.appendChild(img);
+            });
+
+            reader.readAsDataURL(file);
+        } else {
+            pictureImage.innerHTML = pictureImageTxt;
+        }
+        const pictureImage = document.querySelector(".imagem");
+        const pictureImageTxt = "Escolha uma Imagem";
+        pictureImage.innerHTML = pictureImageTxt;
+    });     
+   
+}
---
+++ Projeto Lunary/obj/Release/Package/PackageTmp/Scripts/Menu/ToggleTema.js
@@ -0,0 +1,12 @@
+﻿
+
+
+function Toggle() {
+    const body = document.querySelector("body");
+    const toggle = document.querySelector("#toggle");
+    const sunIcon = document.querySelector(".toggle #sun");
+    const moonIcon = document.querySelector(".toggle #moon");
+        body.classList.toggle("dark");
+        sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+        moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+}
